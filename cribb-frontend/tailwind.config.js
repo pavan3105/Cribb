b01c230fc4084cdf1828100da26897042e4ffc1a
Prajay P <prajayprashanth@gmail.com>
2025-02-10 22:26:56 -0500

    Added signup page design with modals for joinign/creating group

--- cribb-frontend/tailwind.config.js
+++ cribb-frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js" 
   ],
   theme: {
     fontFamily: {
@@ -14,5 +15,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin') 
+  ],
 }
