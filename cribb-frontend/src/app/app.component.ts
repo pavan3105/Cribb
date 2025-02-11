bf2a7d9efc917bc991a52c01ab07b3c2383a9cf3
Prajay P <prajayprashanth@gmail.com>
2025-02-10 22:26:56 -0500

    Added signup page design with modals for joinign/creating group

--- cribb-frontend/src/app/app.component.ts
+++ cribb-frontend/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { initFlowbite } from 'flowbite';
 
 
 
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Add this line
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, LandingComponent], // Add LandingComponent
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
